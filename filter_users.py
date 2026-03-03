551929e687000e763bc963ec30193e99869f04d0
Mikel <hallo.chupi@gmail.com>
2026-03-03 14:49:37 +0100

    Add email filter feature

--- filter_users.py
+++ filter_users.py
@@ -13,3 +13,36 @@ def filter_by_age(users, min_age=None, max_age=None):
         return [u for u in users if u.get("age", 0) <= max_age]
     else:
         return users
+
+
+def filter_by_email(users, email):
+    """
+    Return a list of users whose email matches the given email string.
+    """
+    if not email:
+        return users
+
+    filtered = []
+    for user in users:
+        if user.get("email") == email:
+            filtered.append(user)
+    return filtered
+
+
+def filter_users(users, username=None, country=None, email=None,
+                 min_age=None, max_age=None):
+    """
+    Apply all available filters to the users list.
+    """
+    filtered = users
+
+    if username:
+        filtered = filter_by_username(filtered, username)
+    if country:
+        filtered = filter_by_country(filtered, country)
+    if min_age is not None or max_age is not None:
+        filtered = filter_by_age(filtered, min_age=min_age, max_age=max_age)
+    if email:
+        filtered = filter_by_email(filtered, email)
+
+    return filtered
